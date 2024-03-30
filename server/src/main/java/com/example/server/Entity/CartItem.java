963fb6b05e36f8111e687a053747fa5e15b75ad1
phong28zk <PhongTN.B20VT288@stu.ptit.edu.vn>
2024-03-30 09:55:57 +0700

    (add) Add Cart entity

---
+++ server/src/main/java/com/example/server/Entity/CartItem.java
@@ -0,0 +1,18 @@
+package com.example.server.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;
+    private long count;
+    @ManyToOne
+    private Cart cart;
+    @ManyToOne
+    private Item item;
+}
