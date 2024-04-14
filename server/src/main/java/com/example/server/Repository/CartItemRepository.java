ff839512576c9928b93263374990b19e3c3e7bbd
phong28zk <PhongTN.B20VT288@stu.ptit.edu.vn>
2024-04-15 06:29:18 +0700

    (Refactor) refactor count in profile page

--- server/src/main/java/com/example/server/Repository/CartItemRepository.java
+++ server/src/main/java/com/example/server/Repository/CartItemRepository.java
@@ -14,9 +14,12 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     @Query(value = "select ci.item_id from  Cart_Item as ci where ci.cart_id = :id and ci.status = 'FALSE'", nativeQuery = true)
     List<Long> getFullIdItem(@Param("id") Long id);
 
-    @Query(value = "select ci.count from  Cart_Item as ci where ci.cart_id = :idCart and ci.item_id = :idItem", nativeQuery = true)
+    @Query(value = "select ci.count from Cart_Item as ci where ci.cart_id = :idCart and ci.item_id = :idItem", nativeQuery = true)
     Long getCount(@Param("idCart") Long idCart, @Param("idItem") Long idItem);
 
     @Query(value = "select * from cart_item as ci where ci.item_id = :id and ci.status ='FALSE'", nativeQuery = true)
     CartItem getCartItemsByIdItem(@Param("id") Long id);
+
+    @Query(value = "select ci.count from Cart_Item as ci where ci.item_id = :idItem", nativeQuery = true)
+    List<Long> getCounts(@Param("idItem") Long idItem);
 }
